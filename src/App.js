a7510cf201ee5681e452f2bb91b363751098f14b
yinlainghui <yinlianghui@hotoa.com>
2019-06-20 15:42:34 +0800

    前端使用代理服务接口的方式实现报表功能

--- src/App.js
+++ src/App.js
@@ -1,69 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import styles from "./App.css";
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import ReportList from './components/report-list';
+import ReportDesigner from './components/report-designer';
+import ReportViewer from './components/report-viewer';
 
-function App() {
-  return (
-    <div>
-      <Header />
+import './App.css';
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Topic({ match }) {
-  return <h3>Requested Param: {match.params.id}</h3>;
-}
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:id`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={ReportList} />
+      <Route exact path="/:id" component={ReportViewer} />
+      <Route exact path="/designer/:id" component={ReportDesigner} />
+      <Route exact path="/viewer/:id" component={ReportViewer} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
